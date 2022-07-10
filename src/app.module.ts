cba3cd221581760dfb5199e847bb2071a3efced0
Evva1023 <evoosha@gmail.com>
2022-07-10 15:37:57 +0200

    Connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductController } from './product.controller';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "store",
+      entities: ["dist/**/**.entity{.ts,.js}"],
+      bigNumberStrings: true,
+      logging: true,
+      synchronize: true
+    }),
+  ],
   controllers: [AppController, ProductController],
   providers: [AppService],
 })
