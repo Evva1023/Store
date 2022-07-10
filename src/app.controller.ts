88bdbdc8dc913ef229669564e52ea2ca4dc8f936
Evva1023 <evoosha@gmail.com>
2022-07-10 14:57:38 +0200

    First commit

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get("/")
+  @Render("index")
+  index() {
+    const viewData = [];
+    viewData["title"] = "Always Fresh";
+    return {
+      viewData: viewData
+    };
+  }
+
+  @Get("/about")
+  @Render("about")
+  about() {
+    const viewData = [];
+    viewData["description"] = "This is our about page";
+    viewData["author"] = "Developed by Evvoosha";
+    viewData["title"] = "We Care - We Deliver Fast And Fresh";
+    return {
+      viewData: viewData
+    };
+  }
+}
---
+++ src/product.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Render, Param } from "@nestjs/common";
+
+@Controller("/product")
+export class ProductController {
+    products = [
+        {
+            id: "1",
+            name: "Strawberries",
+            description: "Very sweet and organically grown",
+            image: "one.jpg",
+            price: "10.00"
+        },
+        {
+            id: "2",
+            name: "Blueberries",
+            description: "Very sweet and organically grown",
+            image: "two.jpg",
+            price: "19.00"
+        },
+        {
+            id: "3",
+            name: "Raspberries",
+            description: "Very sweet and organically grown",
+            image: "three.jpg",
+            price: "15.00"
+        },
+        {
+            id: "4",
+            name: "Pineapples",
+            description: "Very sweet and organically grown",
+            image: "four.jpg",
+            price: "10.00"
+        },
+    ]; 
+
+    @Get("/")
+    @Render("products/index")
+    index() {
+        const viewData = [];
+        viewData["title"] = "Our Products";
+        viewData["products"] = this.products;
+        return {
+            viewData: viewData,
+        };
+    }
+
+    @Get("/:id")
+    @Render("products/show")
+    show(@Param() params) {
+        const product = this.products[params.id - 1];
+        const viewData = [];
+        viewData["title"] = product.name;
+        viewData["subtitle"] = product.description;
+        viewData["product"] = product;
+        return {
+            viewData: viewData,
+        };
+    }
+}
